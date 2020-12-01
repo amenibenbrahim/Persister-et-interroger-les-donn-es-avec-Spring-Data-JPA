c8c255318ec8c9bf2e0b93e2a5bf18b0b115d0bd
amenibenbrahim <72887748+amenibenbrahim@users.noreply.github.com>
2020-12-01 02:46:11 +0100

    Add files via upload

---
+++ medicaments/src/main/java/com/ameni/medicaments/repos/MedicamentRepository.java
@@ -0,0 +1,33 @@
+package com.ameni.medicaments.repos;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.ameni.medicaments.entities.Categorie;
+import com.ameni.medicaments.entities.Medicament;
+
+public interface MedicamentRepository extends JpaRepository<Medicament, Long> {
+	
+	List<Medicament> findByNomMedicament(String nom);
+	List<Medicament> findByNomMedicamentContains(String nom);
+	
+	//@Query("select m from Medicament m where m.nomMedicament like %?1 and m.prixMedicament > ?2")
+	//List<Medicament> findByNomPrix (String nom, Double prix);
+	
+	@Query("select m from Medicament m where m.nomMedicament like %:nom and m.prixMedicament > :prix")
+	List<Medicament> findByNomPrix (@Param("nom") String nom,@Param("prix") Double prix);
+
+	@Query("select m from Medicament m where m.categorie = ?1")
+	List<Medicament> findByCategorie (Categorie categorie);
+	
+	List<Medicament> findByCategorieIdCat(Long id);
+	
+	List<Medicament> findByOrderByNomMedicamentAsc();
+	
+	@Query("select m from Medicament m order by m.nomMedicament ASC, m.prixMedicament DESC")
+	List<Medicament> trierMedicamentsNomsPrix ();
+	
+}
